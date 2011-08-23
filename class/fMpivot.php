a348767df556a02e748a6a3ff71fe189c4fb04ae
Mads Føhns <madsfohns@gmail.com>
2011-08-23 21:35:28 +0200

    first commit

---
+++ class/fMpivot.php
@@ -0,0 +1,145 @@
+<?
+	class fMpivot {
+		private $width;
+		private $heightTop;
+		private $heightMinimum;
+		private $focus = 0;
+		private $tabs = array("i","id");
+		private $minimizeString;
+		
+		private $animationSpeed = array(
+			"headerHover"	=> 500,
+			"contentMove"	=> 500
+		);
+		private $header = array(
+			"color"		=> "#808080",
+			"colorHover"=> "#FF0000"
+		);
+		
+		public function __construct($width,$heightTop,$heightMinimum,$minimizeString = true) {
+			$this->width = $width;
+			$this->heightTop = $heightTop;
+			$this->heightMinimum = $heightMinimum;
+			$this->minimizeString = $minimizeString;
+		}
+		
+		public function setAnimationSpeed($headerHover,$contentMove) {
+			if($headerHover != "")
+				$this->animationSpeed["headerHover"] = $headerHover;
+			if($contentMove != "")
+				$this->animationSpeed["contentMove"] = $contentMove;
+		}
+		
+		public function setHeaderColor($color,$colorHover) {
+			if($color != "")
+				$this->header["color"] = $color;
+			if($colorHover != "")
+				$this->header["colorHover"] = $colorHover;
+		}
+		
+		public function setFocus($id) {
+			if(is_int($id)) {
+				if(isset($this->tabs["i"][$id])) {
+					$focus = $id;
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				if(isset($this->tabs["id"][$id])) {
+					$focus = $this->tabs["id"][$id];
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+		
+		public function addTab($id,$title,$content0,$content1,$onclick,$X) {
+			$this->tabs["i"][] = array(
+				$id,
+				$title,
+				$content0,
+				$content1,
+				$onclick,
+				$X
+			);
+			$this->tabs["id"][$id] = count($this->tabs["i"]) - 1;
+		}
+		
+		public function getPivot() {
+			if($this->tabsCount() > 0) {
+				foreach($this->tabs["i"] AS $i => $tab) {
+					unset($attr);
+					
+					list($id,$title,$content0,$content1,$onclick,$X) = $tab;
+					
+					$X["onclick"] = ($onclick == true) ? "fMpivot.gotoTab(%I%);" : $onclick;
+					if($onclick != "")
+						$X["class"] .= ((isset($X["class"])) ? " " : "") . "pivotClickable";
+					
+					if($this->focus == $i)
+						$X["class"] .= ((isset($X["class"])) ? " " : "") . "pivotFocus";
+					
+					if($X != null) {
+						foreach($X AS $a => $b) {
+							$attr .= $a . '="' . $b . '" ';
+						}
+					}
+					
+					$attr = str_replace(array("%I%","%ID%"),array($i,"'" . $id . "'"),$attr);
+					
+					$pivotHeader .= '<span ' . $attr . 'name="pivotHeader" pivoti="' . $i . '" pivotid="' . $id . '">' . $title . '</span>';
+					
+					$content0 = ($content0 == "") ? "&nbsp;" : str_replace(array("%I%","%ID%"),array($i,"'" . $id . "'"),$content0);
+					
+					$content1 = ($content1 == "") ? "&nbsp;" : str_replace(array("%I%","%ID%"),array($i,"'" . $id . "'"),$content1);
+					
+					$pivotContent .= 
+						'<div name="pivotContent" style="width:' . $this->width . 'px;" class="pivotContent' . (($this->focus == $i) ? '' : ' pivotHide') . '" pivoti="' . $i . '" pivotid="' . $id . '">' .
+							'<div name="pivotContent0" class="pivotContent0">' . $content0 . '</div>' .
+							'<div name="pivotContent1" class="pivotContent1">' . $content1 . '</div>' .
+						'</div>';
+				}
+				
+				return $this->minimize(
+					'<div id="pivotHeader">' . 
+						$pivotHeader . 
+					'</div>' .
+					'<div id="pivotContentOuter" style="width:' . $this->width . '">' .
+						'<div id="pivotContent" style="width:' . ($this->tabsCount() * $this->width) . 'px;left:-' . ($this->focus * $this->width) . 'px;">' .
+							$pivotContent .
+						'</div>' .
+					'</div>' .
+					'<div class="pivotClear"></div>'
+				);
+			} else {
+				return "false";
+			}
+		}
+		
+		public function getJavascript() {
+			return $this->minimize(
+				'fMpivot = new fMpivot(
+					' . $this->width . ',
+					' . $this->heightTop . ',
+					' . $this->heightMinimum . ',
+					' . $this->animationSpeed["headerHover"] . ',
+					' . $this->animationSpeed["contentMove"] . ',
+					"' . $this->header["color"] . '",
+					"' . $this->header["colorHover"] . '"
+				);fMpivot.construct();'
+			,"true");
+		}
+		
+		private function tabsCount() {
+			return count($this->tabs["i"]);
+		}
+		
+		private function minimize($string,$force = "default") {
+			if($force == "default")
+				$force = $this->minimizeString;
+				
+			return (((is_bool($force) AND $force == true) || $force == "true") ? str_replace(array("	","\r","\n"),"",$string) : $string);
+		}
+	};
---
+++ js/fMpivot.js
@@ -0,0 +1,144 @@
+var fMpivot;
+function fMpivot(width,heightTop,heightMinimum,headerHover,contentMove,headerColor,headerColorHover) {
+	this.width				= width;
+	this.heightTop			= heightTop;
+	this.heightMinimum		= heightMinimum;
+	this.headerHover		= headerHover;
+	this.contentMove		= contentMove;
+	this.headerColor		= headerColor;
+	this.headerColorHover	= headerColorHover;
+	
+	this.content1showing = false;
+	this.firstRun = true;
+	
+	this.construct = function() {
+		$('#pivotHeader > span.pivotFocus').animate({
+			color: fMpivot.headerColorHover
+		},1);
+		$('#pivotHeader > span').hover(function() {
+				$(this).animate({
+					color: fMpivot.headerColorHover
+				}, this.headerHover);
+		}, function() {
+			if($(this).hasClass('pivotFocus') != true) {
+				$(this).animate({
+					color: fMpivot.headerColor
+				}, this.headerHover);
+			}
+		});
+		
+		this.runI(this.current());
+		
+		this.firstRun = false;
+	};
+	
+	this.heightTopNew = function(heightTopNew) {
+		this.heightTop = heightTopNew;
+		this.resize();
+	};
+	
+	this.current = function() {
+		return parseInt($('#pivotHeader span.pivotFocus').attr("pivoti"));
+	};
+	
+	this.runI = function(i) {
+		var pivotHeader = $('#pivotHeader span[pivoti=' + i + ']');
+		if(typeof(pivotHeader.prop('onclick')) == 'function') {
+			pivotHeader.prop('onclick').call();
+		} else {
+			this.gotoTab(i);
+		}
+	};
+	
+	this.gotoTab = function(tab,callback) {
+		var i;
+		this.content1hide();
+		switch(typeof(tab)) {
+			case "number": {
+				i = tab;
+			}	break;
+			case "string": {
+				i = $('#pivotHeader span[pivotid="' + tab + '"]').attr("pivoti");
+			}	break;
+		}
+		
+		$('#pivotHeader span.pivotFocus[pivoti!="' + i + '"]').removeClass("pivotFocus").animate({
+				color:fMpivot.headerColor
+			},this.headerHover
+		);
+		$('#pivotHeader span[pivoti="' + i + '"]:not(.pivotFocus)').addClass("pivotFocus").animate({
+				color:fMpivot.headerColorHover
+			},this.headerHover
+		);
+		
+		var US = this.resize(true);
+		var theHeight = US[0];
+		var newHeight = US[1];
+		
+		$('#pivotContent div.pivotContent[pivoti="' + i + '"]').removeClass("pivotHide");
+		$('#pivotContent').stop().animate({
+				left: (-this.width * i) + 'px',
+				height: theHeight + "px"
+			},((this.firstRun == true) ? 0 : this.contentMove),function() {
+				$('#pivotContent div[name="pivotContent"][pivoti!="' + i + '"]').addClass("pivotHide");
+				if(typeof(callback) == "function") callback.call();
+			}
+		);
+		$('#pivotContentOuter').stop().animate({
+				height: newHeight + "px"
+			},((this.firstRun == true) ? 0 : this.contentMove),function() {
+				$(this).css("overflowY","auto");
+			}
+		);
+	};
+	this.content1show = function(text) {
+		this.content1showing = true;
+		
+		$('#pivotContent div[name="pivotContent"][pivoti="' + this.current() + '"] div[name="pivotContent0"]').fadeOut(this.contentMove);
+		if(text == undefined) {
+			$('#pivotContent div[name="pivotContent"][pivoti="' + this.current() + '"] div[name="pivotContent1"]').fadeIn(this.contentMove);
+		} else {
+			$('#pivotContent div[name="pivotContent"][pivoti="' + this.current() + '"] div[name="pivotContent1"]').html(text).fadeIn(this.contentMove);
+		}
+		
+		this.resize();
+	};
+	this.content1hide = function() {
+		this.content1showing = false;
+		$('#pivotContent div[name="pivotContent"][pivoti="' + this.current() + '"] div[name="pivotContent0"]:hidden').fadeIn(500);
+		$('#pivotContent div[name="pivotContent"] div[name="pivotContent1"]:visible').fadeOut(500);
+		this.resize();
+	};
+	
+	this.resize = function(returnIt) {
+		var newHeight = parseInt($(window).height() - this.heightTop);
+		if(this.content1showing == false) {
+			var theHeight = $('#pivotContent div[name="pivotContent"][pivoti="' + this.current() + '"] div[name="pivotContent0"]').height();
+		} else {
+			var theHeight = $('#pivotContent div[name="pivotContent"][pivoti="' + this.current() + '"] div[name="pivotContent1"]').height();
+		}
+		
+		if(newHeight < this.heightMin) {
+			newHeight = this.heightMin;
+		}
+		if(newHeight > theHeight) {
+			newHeight = theHeight;
+		}
+		if(returnIt == true) return new Array(theHeight,newHeight);
+		
+		$('#pivotContent').stop().animate(
+			{
+				height: theHeight + "px"
+			},((this.firstRun == true) ? 0 : this.contentMove)
+		);
+		$('#pivotContentOuter').stop().animate(
+			{
+				height: newHeight + "px"
+			},((this.firstRun == true) ? 0 : this.contentMove),function() {
+				$(this).css("overflowY","auto");
+			}
+		);
+	};
+}
+
+$(window).resize(function() {fMpivot.resize();});
