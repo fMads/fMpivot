6152383ee6a8d8350aaab926838646439acbd684
Mads Føhns <madsfohns@gmail.com>
2011-08-23 23:20:17 +0200

    fix $(window).resize for ie8

--- js/fMpivot.js
+++ js/fMpivot.js
@@ -150,6 +150,31 @@ function fMpivot(width,heightTop,heightMinimum,headerHover,contentMove,headerCol
 			}
 		);
 	};
+	
+	this.ieTest = function() {
+		var rv = -1; // Return value assumes failure.
+		if(navigator.appName == 'Microsoft Internet Explorer') {
+			var ua = navigator.userAgent;
+			var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+			if(re.exec(ua) != null)
+				rv = parseFloat(RegExp.$1);
+		}
+		return rv;
+	};
 }
 
-$(window).resize(function() {fMpivot.resize();});
+function getInternetExplorerVersion() {
+	var rv = -1; // Return value assumes failure.
+	if(navigator.appName == 'Microsoft Internet Explorer') {
+		var ua = navigator.userAgent;
+		var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+		if(re.exec(ua) != null)
+			rv = parseFloat(RegExp.$1);
+	}
+	return rv;
+}
+if(getInternetExplorerVersion() == 8) {
+	document.body.onresize = function () {fMpivot.resize();};
+} else {
+	$(window).resize(function() {fMpivot.resize();});
+}
